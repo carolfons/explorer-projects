b95dfe624cf3594c5fdad7a5c5a910718d65739f
CarolFons <carolsantosfonseca@outlook.com>
2024-03-07 16:13:18 -0300

    Projeto 1 finalizado

--- jogo-adivinhacao/main.js
+++ jogo-adivinhacao/main.js
@@ -1,17 +1,36 @@
+
 const randomNumber = Math.round(Math.random()*10)
 let xAttempts = 1
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const tryAgain = document.querySelector(".tryAgain")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 
-function handleClick(){
-    event.preventDefault
+
+//funcao callback
+function handleTryClick(event){
+    event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value)==randomNumber){
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
+        screen1.classList.add("hide")
+        screen2.classList.remove("hide")
 
-        document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
+        screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas!`
     }
 
+    tryAgain.classList.remove('hide')
+    inputNumber.value = ""
     xAttempts++
 }
+
+//eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function(){
+    screen1.classList.remove('hide')
+    screen2.classList.add('hide')
+    tryAgain.classList.add("hide")
+    xAttempts = 1
+})
