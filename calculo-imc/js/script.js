c285b68b9a45109d06795e62c53a04e86da5ccda
CarolFons <carolsantosfonseca@outlook.com>
2024-03-21 17:49:37 -0300

    alert-error

--- calculo-imc/js/script.js
+++ calculo-imc/js/script.js
@@ -1,26 +1,24 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const Modal = {
-
-    wrapper: document.querySelector('.modal-wrapper'),
-    message : document.querySelector('.modal .title span'),
-    btnClose: document.querySelector('.modal button.close'),
-    
-    open() {
-        Modal.wrapper.classList.add('open')
-    },
-    close() {
-        Modal.wrapper.classList.remove('open')
-    }
-}
 
 form.onsubmit = (event) => {
     event.preventDefault()
-    
     const weight = inputWeight.value
     const height = inputHeight.value
+
+    const showAlertError = notNumber(weight) || notNumber(height)
+
+    if(showAlertError){
+        AlertError.open()
+        return;
+    }
+    
+    AlertError.close()
     
     const result = IMC(weight, height)
     
@@ -29,14 +27,13 @@ form.onsubmit = (event) => {
     Modal.message.innerText = message
 
     Modal.open()
-
-}
-
-Modal.btnClose.onclick = ()=>{
-    Modal.close()
 }
 
 function IMC(weight, height){
     return( weight/((height/100)**2)).toFixed(2)
 
+}
+
+function notNumber(value){
+    return isNaN(value) || value == ""
 }
